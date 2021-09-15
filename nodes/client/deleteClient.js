4666a13d774b31d90262d68c6f90fe5804bd0dd2
Maths Physis <smathsphysis@gmail.com>
2021-09-15 10:22:22 -0400

    feat: Adicionado node para deletar clientes

---
+++ nodes/client/deleteClient.js
@@ -0,0 +1,28 @@
+module.exports = function (RED) {
+    function deleteClient(config) {
+        RED.nodes.createNode(this, config);
+
+        this.ozmapconnection = RED.nodes.getNode(config.ozmapconnection);
+        this.status({});
+        this.on('input', async (msg) => {
+            let ozmap = msg.ozmap || this.ozmapconnection.ozmap;
+            if (!ozmap.isConnected()) {
+                msg.payload = 'Ozmap not connected!';
+                this.status({fill: 'red', shape: 'ring', text: 'disconnected'});
+                return this.send([null, msg]);
+            }
+
+            try {
+                msg.payload = await ozmap.getClient().delete(msg.payload.clientId);
+                return this.send([msg, null]);
+
+            } catch (error) {
+                msg.payload = error;
+                return this.send([null, msg]);
+            }
+
+        });
+    }
+
+    RED.nodes.registerType('deleteClient', deleteClient);
+};
